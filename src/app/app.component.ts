54997228514be30644caee6e06b43e3b989510f5
XichTop <xichtop99@gmail.com>
2024-01-17 09:42:20 +0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -7,8 +7,25 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'my-portfolio';
+export class AppComponent implements OnInit {
+
+  currentStyle = 'background: radial-gradient(600px at 158px 748px, rgba(29, 78, 216, 0.15), transparent 80%);';
+
+  constructor(
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit(): void {
+  }
+
+  onMoveMouse(event: MouseEvent) {
+    this.currentStyle = `background: radial-gradient(600px at ${event.clientX}px ${event.clientY}px, rgba(29, 78, 216, 0.15), transparent 80%);`;
+  }
+
+  clickTest() {
+    console.log('clicked');
+  }
+
 }
